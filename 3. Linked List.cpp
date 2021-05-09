7c011752e97a705dde4532ea0b3046a9e5f85bb9
Harsh Deep Ahlawat <63871313+hda9009@users.noreply.github.com>
2021-05-09 22:27:09 +0530

    Update 3. Linked List.cpp

--- 3. Linked List.cpp
+++ 3. Linked List.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <cstring>
 #include <math.h>
 
@@ -16,7 +15,6 @@
 #include <deque>
 #include <iomanip>
 
-// #include <windows.h>
 
 
 using namespace std;
@@ -26,6 +24,17 @@ struct Node
     int data;
     struct Node *next;
 }*actual = NULL;
+
+struct doublyNode
+{
+    struct doublyNode *previous;
+    int data;
+    struct doublyNode *next;
+};
+
+// *************************************************************** //
+// ********************* Creating Node *************************** //
+// *************************************************************** //
 
 Node * createNode(Node* actual, int dummy[], int n)
 {
@@ -51,6 +60,61 @@ Node * createNode(Node* actual, int dummy[], int n)
     return actual;
 }
 
+
+Node * createCircularNode (Node * circularNode, int *a, int n)
+{
+    Node *last = new Node;
+
+    last = circularNode;
+
+    last->data = a[0];
+    last->next = 0;
+
+    for (int i = 1; i < n ; i++)
+    {
+        Node *temp = new Node;
+        temp->data = a[i];
+        temp->next = 0;
+        last->next = temp;
+        last = temp;
+        temp = NULL;
+        delete [] temp;
+    }
+
+    last->next = circularNode;
+    last = NULL;
+    delete [] last;
+
+    return circularNode;
+}
+
+
+doublyNode * createDoublyNode (struct doublyNode * actual, int ar[], int n)
+{
+    doublyNode *last = new doublyNode;
+    actual->previous = 0;
+    actual->data = ar[0];
+    actual->next = 0;
+    
+    last = actual;
+
+    for (int i = 1; i< n; i++)
+    {
+        doublyNode *temp = new doublyNode;
+        temp->previous = last;
+        temp->data = ar[i];
+        temp->next = last->next;  // last->next = 0;
+        last->next = temp;
+        last = temp;
+    }
+
+    return actual;
+}
+
+// *************************************************************** //
+// ********************* display Node *************************** //
+// ************************************************************** //
+
 tuple <int,int> countLinked(struct Node *p)
 {
     static int counts = 0;
@@ -64,16 +128,45 @@ tuple <int,int> countLinked(struct Node *p)
     return make_tuple(sum, counts);
 }
 
-void display(struct Node *p)
+void displaySingleList(struct Node *p)
 {
     if (p!= NULL)
     {
         printf("%d ", p->data);
-        display(p->next);
+        displaySingleList(p->next);
     }
 
     if (p==NULL)    cout << endl;
 }
+
+void displayCircularList(struct Node *head)
+{
+    Node *temp = new Node;
+    temp = head;
+
+    do
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }while(temp != head);
+
+    cout << endl;
+}
+
+void displayDoublyList(struct doublyNode *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+
+    cout << endl;
+}
+
+// *************************************************************** //
+// **************** Operationg on Single Node ******************** //
+// *************************************************************** //
 
 int maximumValue (struct Node *p)
 {
@@ -387,6 +480,7 @@ Node * concatenate(Node *actual, Node *list)
     */
 }
 
+
 int isLoop(Node * actual)
 {
     Node * p = new Node;
@@ -432,12 +526,258 @@ int isLoop(Node * actual)
     return loopExists;
 }
 
+
+Node * mergingLists(Node *actual, Node *list)
+{
+    Node *temp = new Node;
+    temp = list;
+    int movePoint = 2;
+
+    while (movePoint > 0)
+    {
+        movePoint--;
+        actual = actual->next;
+    }
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = actual;
+
+    return list;
+}
+
+int intersectionPoint(Node *actual, Node *list)
+{
+    int *stackAddrActual = new int[1];
+    
+    int j = 0;
+    while ( (actual != NULL) )
+    {
+        int *stackTemp = new int[j+1];
+
+        for (int i=0; i< (j); i++)
+        {
+            stackTemp[i] = stackAddrActual[i];
+
+        }
+        stackTemp[j] = (int)(actual);
+        stackAddrActual = stackTemp;
+        for (int i=0; i<= (j); i++)
+        {
+            cout << stackAddrActual[i] << ", " << stackTemp[i] << "\t";
+        }
+        stackTemp = NULL;
+        delete [] stackTemp; 
+        j++;
+        cout << endl;
+        actual = actual->next;
+    }
+
+}
+
+// *************************************************************** //
+// **************** Operationg on Circular Node ****************** //
+// *************************************************************** //
+
+
+
+
+// *************************************************************** //
+// **************** Operationg on Doubly Node ****************** //
+// *************************************************************** //
+
+void insertElementDoublyList(struct doublyNode *head, int element, int position)
+{
+    struct doublyNode *temp = new doublyNode;
+    temp->next = 0;
+    temp->data = element;
+    temp->previous = 0;
+    
+    struct doublyNode *follow = new doublyNode;
+    struct doublyNode *lead = new doublyNode;  
+    follow = head;
+    lead = head;
+
+    if (position == 0)
+    {
+        temp->next = lead;
+        lead->previous = temp;
+        lead = temp;
+    }
+    else if (position >= 1) 
+    {
+        position--;
+        lead = lead->next;
+    }  
+
+    while ( (position > 0) && (lead != NULL) )
+    {
+        position--;
+        lead = lead->next;
+        follow = follow->next;
+    }
+    if ( (position == 0) || (lead == NULL) )
+    {     // after 1st position or any other position
+        follow->next = temp;
+        temp->next = lead;
+    }
+
+    temp = NULL;
+    follow = NULL;
+    lead = NULL;
+
+    delete [] temp;
+    delete [] follow;
+    delete [] lead;
+}
+
+struct doublyNode * deleteElementDoublyList(struct doublyNode *head, int element)
+{
+#if 0   // This is using three pointers
+    struct doublyNode *follow = new doublyNode;
+    struct doublyNode *lead = new doublyNode;  
+    follow = head;
+    lead = head;
+
+    // follow - head - lead -> sequence follow
+    int elementFound = 0;
+    if (head->data == element)  // the first element
+    {
+        lead = lead->next;
+        head->data = 0;
+        head->next = 0;
+        head = lead;
+        elementFound = 1;
+    }
+    else
+    {
+        lead = lead->next;
+        lead = lead->next;
+        head = head->next;
+    }
+    while  ( (head->data != element) && (lead != NULL) && (!elementFound) )
+    {           // checking starts with second element
+        
+        if (lead->next == NULL) 
+        {       // checking the last element if matched
+            lead->previous = 0;
+            lead->data = 0;
+            head->next = 0;
+            elementFound = 1;
+            break;
+        }
+        follow = follow->next;
+        head = head->next;
+        lead = lead->next;
+    }
+
+    if ( (head->data == element) && (!elementFound) )  // when head is found the element
+    {
+        
+        follow->next = lead;
+        lead->previous = follow;
+        head->previous = 0;
+        head->data = 0;
+        head->next = 0;
+    }
+
+    follow = NULL;
+    lead = NULL;
+
+    delete [] follow;
+    delete [] lead;
+    return head;
+#endif
+
+    // With a single pointer we can do it
+
+    struct doublyNode *lead = new doublyNode;  
+    lead = head;
+
+    while ( (lead->data != element) && (lead->next != NULL) )
+    {
+        lead = lead->next;
+    }
+
+    if (lead->data == element)
+    {
+        if (lead->previous == NULL)
+        {   // This is for first node
+            
+            lead = lead->next;
+            lead->previous = 0;
+            head = lead;
+        }
+        else if (lead->next == NULL)
+        {   // This is for last node
+            lead->previous->next = 0;
+        }
+        else
+        {
+            lead->previous->next = lead->next;
+            lead->next->previous = lead->previous;
+        }
+    }
+
+    return head;
+
+}
+
+struct doublyNode * reverseDoublyList(struct doublyNode *head)
+{
+    
+    struct doublyNode *temp = new doublyNode;
+    temp = head->next;
+
+    while ( ( head != NULL) && (temp->next != NULL) )
+    {
+        
+        if (head->previous == NULL)
+        {
+            head->next = 0;
+            head->previous = temp;
+            head = head->previous;
+            temp = temp->next;
+        }
+        else
+        {
+            head->next = head->previous;
+            head->previous = temp;
+            head = head->previous;
+            temp = temp->next;
+        }
+    }
+    if (temp->next == NULL)
+    {
+        head->next = head->previous;
+        head->previous = temp;
+
+        temp->next = head;
+        temp->previous = 0;
+    }
+    
+    head = temp;
+
+        temp = NULL;
+        delete [] temp;
+
+    return head;
+}
+
+// *************************************************************** //
+// ********************** main *********************************** //
+// *************************************************************** //
+
 int main()
 {
+    
     int choiceSelection = 0;
     int dummyArray[] = {20, 10, 200, 20, 10, 200, 4 , 6};
     actual = createNode(actual, dummyArray, sizeof(dummyArray)/sizeof(int));
-    display(actual);
+    displaySingleList(actual);
     cout << endl << endl;
     while (1)
     {
@@ -453,7 +793,12 @@ int main()
                 "9.  Removing the Duplicate Elements in the list \n" <<
                 "10. Reversing the List \n" <<
                 "11. Concatenate the List \n" <<
-                "12. Check if the Linked List in the Loop or not \n"
+                "12. Check if the Linked List in the Loop or not \n" <<
+                "13. Intersection of two Linked List and finding the intersection point \n" <<
+                "\n" <<
+                "14. Circular Linked List? \n" <<
+                "15. Doubly Linked List? \n" <<
+                "16. Doubly Circular Linked List? \n" <<
                 "\nEnter your choice: ";
 
         cin >> choiceSelection ; 
@@ -495,7 +840,7 @@ int main()
 
                 actual = moveToHead(actual, searchKeyword);
                 cout << endl;
-                display(actual);
+                displaySingleList(actual);
                 
                 break;
             }
@@ -509,7 +854,7 @@ int main()
                 cin >> insertPosition;
 
                 actual = elementInsertion(actual, insertElement, insertPosition);
-                display(actual);
+                displaySingleList(actual);
 
                 break;
             }
@@ -520,7 +865,7 @@ int main()
                 cout << "Enter the number you want to insert: " ;
                 cin >> insertElement;
                 appendElement(actual, insertElement);
-                display(actual);
+                displaySingleList(actual);
 
                 break;
             }
@@ -531,7 +876,7 @@ int main()
                 cout << "Enter the number you want to insert: " ;
                 cin >> element;
                 actual = deleteElement(actual, element);
-                display(actual);
+                displaySingleList(actual);
 
                 break;
             }
@@ -539,28 +884,28 @@ int main()
             case 7:
             {
                 popElement(actual);
-                display(actual);
+                displaySingleList(actual);
                 break;
             }
 
             case 8:
             {
                 sortingElement(actual);
-                display(actual);
+                displaySingleList(actual);
                 break;
             }
 
             case 9:
             {
                 duplicateElement(actual);
-                display(actual);
+                displaySingleList(actual);
                 break;
             }
 
             case 10:
             {
                 reverseElement(actual);
-                display(actual);
+                displaySingleList(actual);
                 break;
             }
             
@@ -570,7 +915,7 @@ int main()
                 Node * list2 = new Node;
                 list2 = createNode(list2, arr, sizeof(arr)/ sizeof(int));
                 concatenate(actual, list2);
-                display(actual);
+                displaySingleList(actual);
                 break;
             }
 
@@ -583,7 +928,7 @@ int main()
                 list2 = createNode(list2, arr, sizeof(arr)/ sizeof(int));
                 concatenate(actual, list2);
 
-                display(actual);
+                displaySingleList(actual);
 
                 int condition = isLoop(actual);
                 cout << endl;
@@ -592,6 +937,125 @@ int main()
                 else
                     cout << "Looping does not Exists" << endl;
 
+                break;
+            }
+
+            case 13:
+            {
+                cout << "Manually merging the list in actual list " << endl;
+                
+                int arr[] = {901, 902, 903,905, 906, 908, 998};
+                Node * list2 = new Node;
+                list2 = createNode(list2, arr, sizeof(arr)/ sizeof(int));
+                
+                list2 = mergingLists(actual, list2);
+
+                displaySingleList(list2);
+
+                intersectionPoint(actual, list2);
+                break;
+            }
+            case 14:
+            {
+                static int repeat;
+
+                static Node *circularNode = new Node;
+                if (!repeat)
+                {
+                    int arr[] = {1,2,3,4, 5, 6, 7, 8};
+                    circularNode = createCircularNode (circularNode, arr, sizeof(arr)/ sizeof(int));
+                    displayCircularList(circularNode);
+                    repeat++;
+                }
+
+                int choiceSelection = 0;
+                cout << "\n\nEnter the operations you want to perform on Circular Linked List: \n" <<
+                        "1.  Inserting an Element in the list \n" <<
+                        "2.  Deleting an Element from the list \n" <<
+                        "\nEnter your choice: ";
+                cin >> choiceSelection ; 
+
+                switch (choiceSelection)
+                {
+                    case 1:
+                    {
+                        int insertPosition = 0;
+                        int insertElement = 0;
+                        cout << "Enter the Element you want to insert: ";
+                        cin >> insertElement;
+                        cout << "Enter the position you want to insert the element: ";
+                        cin >> insertPosition;
+
+                        // insertElementCircularList(circularNode, insertElement, insertPosition);
+
+                        displayCircularList(circularNode);
+
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+            case 15:
+            {
+                static int repeat;
+                
+                static doublyNode *doubleNode = new doublyNode;
+                if (!repeat)
+                {
+                    int arr[] = {1,2,3,4,5,6,7,8};
+                    doubleNode = createDoublyNode (doubleNode, arr, sizeof(arr)/ sizeof(int));
+                    repeat++;
+                }
+
+                displayDoublyList(doubleNode);
+
+                int choiceSelection = 0;
+                cout << "\n\nEnter the operations you want to perform on Circular Linked List: \n" <<
+                        "1.  Inserting an Element in the list \n" <<
+                        "2.  Deleting an Element from the list \n" <<
+                        "3.  Reversing the Element in the list \n" <<
+                        "\nEnter your choice: ";
+                cin >> choiceSelection ; 
+
+                switch (choiceSelection)
+                {
+                    case 1:
+                    {
+                        int insertPosition = 0;
+                        int insertElement = 0;
+                        cout << "Enter the Element you want to insert: ";
+                        cin >> insertElement;
+                        cout << "Enter the position you want to insert the element: ";
+                        cin >> insertPosition;
+
+                        insertElementDoublyList(doubleNode, insertElement, insertPosition);
+
+                        displayDoublyList(doubleNode);
+
+                        break;
+                    }
+
+                    case 2:
+                    {
+                        int insertElement = 0;
+                        cout << "Enter the Element you want to delete: ";
+                        cin >> insertElement;
+                        doubleNode = deleteElementDoublyList(doubleNode, insertElement);
+                        displayDoublyList(doubleNode);
+                        break;
+                    }
+
+                    case 3:
+                    {
+                        doubleNode = reverseDoublyList(doubleNode);
+                        displayDoublyList(doubleNode);
+                        break;
+                    }
+                }
+
+                break;
             }
         }
 
