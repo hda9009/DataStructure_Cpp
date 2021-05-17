68fe745b9e8e2c6467ff86cf7918bdd80a1eb1ff
Harsh Deep Ahlawat <63871313+hda9009@users.noreply.github.com>
2021-05-17 12:36:38 +0530

    Update 6. BinaryTrees.cpp

--- 6. BinaryTrees.cpp
+++ 6. BinaryTrees.cpp
@@ -21,6 +21,7 @@
 
 using namespace std;
 
+
 struct treeNode
 {
     treeNode *left;
@@ -40,10 +41,27 @@ struct Queue
     bool isEmpty(void);
     void enqueue(treeNode *node);
     treeNode * dequeue(void);
-    
+
 }*headQueue, *tempQueue, *tempDequeue;
 
+
+struct Stack
+{
+    Stack *previous;
+    int data;
+    Stack *next;
+
+    int counts = 0;
+    bool isEmpty(void);
+}*headStack, *tempStack;
+
+
+
 void createTree();
+void preOrderIterative(treeNode *head);
+treeNode* popStack(Stack *head);
+void levelTraversal(treeNode *head);
+
 
 
 void createTree()
@@ -58,20 +76,21 @@ void createTree()
 
     headQueue->enqueue(Root);      // Insert the address in the queue
 
-    while (headQueue->isEmpty())
+    while (!headQueue->isEmpty())
     {
         presentNode = headQueue->dequeue();  // presentNode = Root;
 
-        char leftNodePresent = 0;
-        char rightNodePresent = 0;
-        cout << "Left Node present?: Y/N): ";
+        cout << "\nEnter Child Value for Node: " << presentNode->data << endl;
+        int leftNodePresent = 0;
+        int rightNodePresent = 0;
+        cout << "Left Node present?: 1/0): ";
         cin >> leftNodePresent;
 
         
         
         switch (leftNodePresent)
         {
-            case 'Y':
+            case 1:
             {
                 treeNode *checkDummy = new treeNode;  // Requires to reinitialize again
                 cout << "Enter the value of Left Child Node: ";
@@ -87,19 +106,19 @@ void createTree()
                 break;
             }
 
-            case 'N':
+            case 0:
             {
                 presentNode->left = 0;
                 break;
             }
         }
 
-        cout << "Right Node present?: Y/N): ";
+        cout << "Right Node present?: 1/0: ";
         cin >> rightNodePresent;
 
         switch (rightNodePresent)
         {
-            case 'Y':
+            case 1:
             {
                 treeNode *checkDummy = new treeNode;  // Requires to reinitialize again
 
@@ -116,7 +135,7 @@ void createTree()
                 break;
             }
 
-            case 'N':
+            case 0:
             {
                 presentNode->right = 0;
                 break;
@@ -172,12 +191,13 @@ treeNode * Queue::dequeue(void)
 
 bool Queue::isEmpty(void)
 {
-    if (headQueue->size != headQueue->counts)   return 1;
-    else                                        return 0;
+    if (headQueue->size != (headQueue->counts ))   return 0;
+    else                                        return 1;
 }
 
 void preOrder(treeNode *head)
 {
+
     if (head != NULL)
     {
         cout << head->data << " ";
@@ -185,6 +205,238 @@ void preOrder(treeNode *head)
         preOrder(head->right);
     }
 }
+
+void inOrder(treeNode *head)
+{
+
+    if (head != NULL)
+    {
+        inOrder(head->left);
+        cout << head->data << " ";
+        inOrder(head->right);
+    }
+}
+
+void postOrder(treeNode *head)
+{
+
+    if (head != NULL)
+    {
+        inOrder(head->left);
+        inOrder(head->right);
+        cout << head->data << " ";
+    }
+}
+
+void pushStack(treeNode *headtree)
+{
+    if (!headStack->counts)
+    {
+        headStack->previous = 0;
+        headStack->data = (int)headtree;
+        headStack->next = 0;
+        tempStack = headStack;
+        headStack->counts++;
+    }
+    else
+    {
+        Stack *dummy = new Stack;
+        dummy->previous = tempStack;
+        dummy->data = (int)headtree;
+        dummy->next = 0;
+        tempStack->next = dummy;
+        tempStack = dummy;
+
+        headStack->counts++;
+
+        dummy = NULL;
+        delete [] dummy;
+    }
+}
+
+treeNode* popStack(Stack *head)
+{
+    if (headStack->counts == 0)
+    {
+        return NULL;
+    }
+
+    headStack->counts--;
+
+    if (head->next != NULL)
+    {
+        head = head->next;
+
+        while (head->next != NULL)
+        {
+            head = head->next;
+            headStack = headStack->next;
+        }
+        int value = head->data;
+
+        headStack->next = 0;        // remove the next node;
+
+        head = NULL;
+        
+        return (treeNode*)(value);
+    }
+    
+    return (treeNode*)(headStack->data);
+}
+
+bool Stack::isEmpty(void)
+{
+    if (headStack->counts == 0) return 1;
+    else                return 0;
+}
+
+void preOrderIterative(treeNode *head)
+{
+    while ( (head != NULL) || ( !headStack->isEmpty() )  )
+    {
+        cout << head->data << " ";
+        if (head->left != NULL)
+        {
+            pushStack(head);
+            head = head->left;
+        }
+        else if (head->right != NULL)
+        {
+            pushStack(head);
+            head = head->right;
+        }   
+        else
+        {
+            head = popStack(headStack);
+
+            if (head == NULL){
+                break;
+            }
+
+            if (head->right != NULL)
+            {
+                head = head->right;   
+            }      
+        }
+    }
+}
+
+void inOrderIterative(treeNode *head)
+{
+    while ( (head != NULL) || ( !headStack->isEmpty() )  )
+    {
+        if (head->left != NULL)
+        {
+            pushStack(head);
+            head = head->left;
+        }
+        else if (head->right != NULL)
+        {
+            pushStack(head);
+            head = head->right;
+        }   
+        else
+        {
+            cout << head->data << " ";
+            head = popStack(headStack);
+            if (head == NULL){
+                break;
+            }
+
+            if (head->right != NULL)
+            {
+                cout << head->data << " ";
+                head = head->right;   
+            }      
+        }
+    }
+}
+
+void levelTraversal(treeNode *head)
+{
+
+}
+
+
+int countNode(treeNode *head)
+{
+    int lChild=0, rChild= 0;
+
+    if (head != NULL)
+    {
+        if (head->left != NULL)
+            lChild = countNode(head->left);
+        if (head->right != NULL)
+            rChild = countNode(head->right);
+
+        return (lChild + rChild + 1);
+    }
+    return 0;
+}
+
+int sumNode(treeNode *head)
+{
+    int lChild = 0, rChild = 0, sum = 0;
+    
+    if (head != NULL)
+    {
+        if (head->left != NULL)
+            lChild = sumNode(head->left);
+        if (head->right != NULL)
+            rChild = sumNode(head->right);
+
+        return (lChild + rChild + head->data);
+    }
+    return 0;
+} 
+
+int countLeafNodes(treeNode *head)
+{
+    int lChild = 0, rChild = 0;
+    if (head != NULL)
+    {
+        if (head->left != NULL)
+        {
+            lChild = sumNode(head->left);
+        }    
+
+        if (head->right != NULL)
+        {    
+            rChild = sumNode(head->right);
+        }    
+
+        if (head->left == NULL && head->right == NULL)
+        {
+            return (lChild + rChild + 1);
+        }
+
+        return 0;
+    }
+} 
+
+int countNonLeafNodes(treeNode *head)
+{
+    int lChild = 0, rChild = 0;
+    if (head != NULL)
+    {
+        if (head->left != NULL)
+        {
+            lChild = sumNode(head->left);
+        }    
+
+        if (head->right != NULL)
+        {    
+            rChild = sumNode(head->right);
+        }    
+
+        if ( (head->left == NULL && head->right != NULL) || (head->left != NULL || head->right == NULL)  )
+        {
+            return (lChild + rChild + 1);
+        }
+
+    }
+    return 0;
+} 
 
 int main()
 {
@@ -197,6 +449,23 @@ int main()
     cin >> headQueue->size;
 
     createTree();
-    preOrder(Root);
+    // cout << "PreOrder: ";
+    // preOrder(Root);
+    // cout << "\nInOrder: ";
+    // inOrder(Root);
+    // cout << "\nPostOrder: ";
+    // postOrder(Root);
 
+    headStack = new Stack;
+
+    cout << "\nPreOrder: ";
+    preOrderIterative(Root);
+
+    cout << "\nPreOrder: ";
+    inOrderIterative(Root);
+
+    cout << "\nTotal Number of Nodes in the Tree are: " << countNode(Root);
+    cout << "\nTotal Sum of Nodes in the Tree are: " << sumNode(Root);
+    cout << "\nLeaf nodes in the Tree are: " << countLeafNodes(Root);
+    cout << "\nNon-Leaf nodes with degree 1 in the Tree are: " << countNonLeafNodes(Root);
 }
